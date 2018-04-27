749ff1b5149f12612e5998427943bc32f2096062
vinot <vinot@LAPTOP-B3M92C9H>
2018-04-27 07:26:00 +0530

    initial commit

---
+++ springbatch/src/main/java/workout/springbatch/App.java
@@ -0,0 +1,37 @@
+package workout.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException
+    {
+    	String[] springConfig  = 
+			{	"database.xml", 
+				"repository.xml",
+				"user_job.xml" 
+			};
+	ApplicationContext context = 
+			new ClassPathXmlApplicationContext(springConfig);
+			
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("userjob");
+		JobExecution execution = jobLauncher.run(job, new JobParameters());
+		System.out.println("Exit Status : " + execution.getStatus());
+    }
+
+	
+}
