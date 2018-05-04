37f4701cecd73931f9db58f7ed2e012c58c6ebea
vinothvs <vinothvs@hotmail.com>
2018-05-04 08:48:26 +0530

    initial commit

---
+++ springwebapp/src/main/java/workout/springwebapp/App.java
@@ -0,0 +1,13 @@
+package workout.springwebapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
