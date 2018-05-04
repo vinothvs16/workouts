ad56c104d19c9e9ea4de2000d739545ba87b7078
vinothvs <vinothvs@hotmail.com>
2018-05-04 09:07:29 +0530

    id property set method commented

--- springwebapp/src/main/java/workout/springwebapp/processors/StringArrayUserProcessor.java
+++ springwebapp/src/main/java/workout/springwebapp/processors/StringArrayUserProcessor.java
@@ -8,9 +8,9 @@ public class StringArrayUserProcessor implements ItemProcessor<String[],User>{
 
 	public User process(String[] arg0) throws Exception {
 		User user = new User();
-		user.setId(Integer.valueOf(arg0[0]));
-		user.setName(arg0[1]);
-		user.setAge(Integer.valueOf(arg0[2]));
+		//user.setId(Integer.valueOf(arg0[0]));
+		user.setName(arg0[0]);
+		user.setAge(Integer.valueOf(arg0[1]));
 		return user;
 	}
 
